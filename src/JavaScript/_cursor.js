4085d00ed709af3c86aa96e014d6ebea4e449336
Aryan-81 <llaryanllrajll@gmail.com>
2023-12-27 23:02:26 +0530

    _cursor.js update

--- src/JavaScript/_cursor.js
+++ src/JavaScript/_cursor.js
@@ -1,17 +1,29 @@
-const cursor = document.querySelector(".cursor");
-var timeout;
+const cursor = document.querySelector(".cursor");  // Select the cursor element from the DOM
+let timeout;  // Declare a variable to store the timeout ID
 
-document.addEventListener("mousemove", (pos) => {
-    let x = pos.pageX;
-    let y = pos.pageY;
+// Event listener for mouse movement
+document.addEventListener("mousemove", (event) => {
+    // Get the mouse coordinates
+    const x = event.pageX;
+    const y = event.pageY;
+
+    // Update the cursor position
     cursor.style.top = y + "px";
     cursor.style.left = x + "px";
     cursor.style.display = "block";
-    function mousestopped() {
+
+    // Clear the timeout if mouse movement occurs
+    clearTimeout(timeout);
+
+    // Set a new timeout for hiding the cursor after 1000 milliseconds (1 second)
+    timeout = setTimeout(() => {
         cursor.style.display = "none";
-    }
-    timeout = setTimeout(mousestopped, 1000);
+    }, 1000);
 });
+
+// Event listener for mouse leaving the document
 document.addEventListener("mouseout", () => {
+    // Hide the cursor when the mouse leaves the document
     cursor.style.display = "none";
-})
+});
+
