a873c25164b218b14febe5eefe4e17d66fa28cc4
Aryan-81 <llaryanllrajll@gmail.com>
2023-12-29 13:49:03 +0530

    adding sub web pages

---
+++ src/JavaScript/display_team.js
@@ -0,0 +1,13 @@
+
+function changeHeading(event) {
+    // Prevent the default behavior of the anchor tag
+    event.preventDefault();
+
+    // Get the clicked year from the anchor tag's text content
+    var clickedYear = event.target.textContent;
+
+    // Update the h2 element with the clicked year
+    document.getElementById('teamHeading').textContent = clickedYear;
+  }
+
+
