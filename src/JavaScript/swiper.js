e5ea3fcd4cce16a0afc3285665822e1df2452715
Aryan-81 <llaryanllrajll@gmail.com>
2024-02-02 20:03:49 +0530

    changing the blog-slider

--- src/JavaScript/swiper.js
+++ src/JavaScript/swiper.js
@@ -1,26 +1,12 @@
-let currentSlide = 0;
-
-function showSlide(index) {
-  const swiperWrapper = document.querySelector('.swiper-wrapper');
-  const slides = document.querySelectorAll('.swiper-slide');
-  const slideWidth = slides[0].offsetWidth;
-
-  currentSlide = index;
-
-  swiperWrapper.style.transform = `translateX(-${slideWidth * index}px)`;
-}
-
-function nextSlide() {
-  const slides = document.querySelectorAll('.swiper-slide');
-  if (currentSlide < slides.length - 1) {
-    currentSlide++;
-    showSlide(currentSlide);
-  }
-}
-
-function prevSlide() {
-  if (currentSlide > 0) {
-    currentSlide--;
-    showSlide(currentSlide);
-  }
-}
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
